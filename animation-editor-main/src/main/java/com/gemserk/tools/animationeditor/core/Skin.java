2898d4541a5bf0862290b2125782a08cd6861a2a
Ariel Coppes <ariel.coppes@gemserk.com>
2011-12-16 11:50:39 -0200

    added spatial to SkinPatch to be able to modify the sprite stuff like x,y,w,h and angle

--- animation-editor-main/src/main/java/com/gemserk/tools/animationeditor/core/Skin.java
+++ animation-editor-main/src/main/java/com/gemserk/tools/animationeditor/core/Skin.java
@@ -6,14 +6,19 @@ import java.util.Map;
 import java.util.Set;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.gemserk.commons.gdx.graphics.SpriteUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.gemserk.commons.gdx.games.Spatial;
+import com.gemserk.commons.gdx.games.SpatialImpl;
 
 public class Skin {
 
 	public static class SkinPatch {
-		
+
 		Joint joint;
 		Sprite sprite;
+
+		Spatial spatial;
+		Vector2 center;
 
 		public Joint getJoint() {
 			return joint;
@@ -26,35 +31,53 @@ public class Skin {
 		SkinPatch(Joint joint, Sprite sprite) {
 			this.joint = joint;
 			this.sprite = sprite;
+			this.spatial = new SpatialImpl(joint.getX(), joint.getY(), sprite.getWidth(), sprite.getHeight(), 0f);
+			this.center = new Vector2(0.5f, 0.5f);
 		}
-		
+
 		void update() {
-			SpriteUtils.centerOn(sprite, joint.getX(), joint.getY(), 0.5f, 0.5f);
-			sprite.setRotation(joint.getAngle());
+
+			sprite.setRotation(spatial.getAngle());
+
+			float ox = spatial.getWidth() * center.x;
+			float oy = spatial.getHeight() * center.y;
+
+			if (ox != sprite.getOriginX() || oy != sprite.getOriginY())
+				sprite.setOrigin(ox, oy);
+
+			if (sprite.getWidth() != spatial.getWidth() || sprite.getHeight() != spatial.getHeight())
+				sprite.setSize(spatial.getWidth(), spatial.getHeight());
+
+			float x = spatial.getX() - sprite.getOriginX();
+			float y = spatial.getY() - sprite.getOriginY();
+
+			if (x != sprite.getX() || y != sprite.getY())
+				sprite.setPosition(x, y);
+
 		}
-		
+
 	}
-	
+
 	Map<String, SkinPatch> patches;
 	ArrayList<SkinPatch> patchList;
-	
+
 	public Skin() {
 		patches = new HashMap<String, Skin.SkinPatch>();
 		patchList = new ArrayList<Skin.SkinPatch>();
 	}
-	
+
 	public void addPatch(Joint joint, Sprite sprite) {
 		SkinPatch patch = new SkinPatch(joint, sprite);
-		
+
 		if (patches.containsKey(joint.getId())) {
 			SkinPatch previousPatch = patches.get(joint.getId());
 			patchList.remove(previousPatch);
 		}
-		
+
 		patches.put(joint.getId(), patch);
 		patchList.add(patch);
 	}
-	
+
 	public void removePatch(Joint joint) {
 		ArrayList<Joint> joints = JointUtils.toArrayList(joint);
 		for (int i = 0; i < joints.size(); i++) {
@@ -63,7 +86,7 @@ public class Skin {
 			patchList.remove(patch);
 		}
 	}
-	
+
 	public void update() {
 		Set<String> keySet = patches.keySet();
 		for (String jointId : keySet) {
@@ -71,17 +94,17 @@ public class Skin {
 			skinPatch.update();
 		}
 	}
-	
+
 	public int patchesCount() {
 		return patchList.size();
 	}
-	
+
 	public SkinPatch getPatch(int index) {
 		return patchList.get(index);
 	}
-	
+
 	public SkinPatch getPatch(String jointId) {
 		return patches.get(jointId);
 	}
-	
+
 }
