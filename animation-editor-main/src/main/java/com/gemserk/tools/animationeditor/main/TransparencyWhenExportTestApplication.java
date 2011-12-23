7baf5ba6e5c20ae3598a4dffd8c1d0ffe7519474
Ariel Coppes <ariel.coppes@gemserk.com>
2011-12-23 15:01:55 -0200

    changed to use libgdx Mesh and GL14 blendfunc with new values

--- animation-editor-main/src/main/java/com/gemserk/tools/animationeditor/main/TransparencyWhenExportTestApplication.java
+++ animation-editor-main/src/main/java/com/gemserk/tools/animationeditor/main/TransparencyWhenExportTestApplication.java
@@ -9,7 +9,14 @@ import org.lwjgl.opengl.GL14;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.Mesh.VertexDataType;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.VertexAttribute;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.gemserk.commons.gdx.ApplicationListenerGameStateBasedImpl;
 import com.gemserk.commons.gdx.graphics.Mesh2d;
 import com.gemserk.commons.gdx.graphics.Mesh2dBuilder;
@@ -22,6 +29,10 @@ public class TransparencyWhenExportTestApplication {
 		private Mesh2d mesh;
 		private Texture texture1;
 		private Texture texture2;
+		private Sprite sprite1;
+		private Sprite sprite2;
+		private Sprite sprite3;
+		private Mesh renderMesh;
 
 		@Override
 		public void create() {
@@ -54,11 +65,42 @@ public class TransparencyWhenExportTestApplication {
 
 			texture1 = new Texture(Gdx.files.internal("alphatest/body-top.png"));
 			texture2 = new Texture(Gdx.files.internal("alphatest/head.png"));
+
+			sprite1 = new Sprite(texture1);
+			sprite2 = new Sprite(texture2);
+			sprite3 = new Sprite(texture2);
+
+			int size = 100;
+			renderMesh = new Mesh(VertexDataType.VertexArray, false, size * 4, size * 6, new VertexAttribute(Usage.Position, 2, ShaderProgram.POSITION_ATTRIBUTE), // 
+					new VertexAttribute(Usage.ColorPacked, 4, ShaderProgram.COLOR_ATTRIBUTE), // 
+					new VertexAttribute(Usage.TextureCoordinates, 2, ShaderProgram.TEXCOORD_ATTRIBUTE + "0"));
+			
+			int len = size * 6;
+			short[] indices = new short[len];
+			short j = 0;
+			for (int i = 0; i < len; i += 6, j += 4) {
+				indices[i + 0] = (short)(j + 0);
+				indices[i + 1] = (short)(j + 1);
+				indices[i + 2] = (short)(j + 2);
+				indices[i + 3] = (short)(j + 2);
+				indices[i + 4] = (short)(j + 3);
+				indices[i + 5] = (short)(j + 0);
+			}
+			renderMesh.setIndices(indices);
+
 		}
 
 		@Override
 		public void render() {
 			super.render();
+
+			sprite1.setPosition(0f, 0f);
+			sprite2.setPosition(-0.3f, 0f);
+			sprite3.setPosition(0.2f, 0f);
+			
+			sprite1.setSize(0.5f, 0.5f);
+			sprite2.setSize(0.5f, 0.5f);
+			sprite3.setSize(0.5f, 0.5f);
 
 			// GL20 gl20 = Gdx.graphics.getGL20();
 
@@ -79,16 +121,37 @@ public class TransparencyWhenExportTestApplication {
 
 			// GL11.glBlendFunc(GL11.GL_ONE, GL11.GL_SRC_ALPHA);
 
-			GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+			GL11.glClearColor(0.0f, 0f, 0f, 0.0f);
 			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 
 			GL11.glEnable(GL11.GL_BLEND);
 
-			GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_ONE, GL11.GL_ONE_MINUS_SRC_ALPHA);
+			GL14.glBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, GL11.GL_SRC_ALPHA, GL11.GL_DST_ALPHA);
 
-			renderMesh(mesh, texture2, -0.3f, 0f);
-			renderMesh(mesh, texture1, 0f, 0f);
-			renderMesh(mesh, texture2, 0.2f, 0f);
+			Gdx.graphics.getGL10().glEnable(GL10.GL_TEXTURE_2D);
+			
+			sprite1.getTexture().bind();
+			
+			renderMesh.setVertices(sprite1.getVertices());
+			renderMesh.render(GL10.GL_TRIANGLES, 0, 1 * 6);
+			
+			sprite2.getTexture().bind();
+			
+			renderMesh.setVertices(sprite2.getVertices());
+			renderMesh.render(GL10.GL_TRIANGLES, 0, 1 * 6);
+			
+			sprite3.getTexture().bind();
+			
+			renderMesh.setVertices(sprite3.getVertices());
+			renderMesh.render(GL10.GL_TRIANGLES, 0, 1 * 6);
+			
+			// mesh.render(GL10.GL_TRIANGLES, 0, spritesInBatch * 6);
+
+			// sprite1.getVertices();
+
+//			renderMesh(mesh, texture2, -0.3f, 0f);
+//			renderMesh(mesh, texture1, 0f, 0f);
+//			renderMesh(mesh, texture2, 0.2f, 0f);
 
 			if (Gdx.input.justTouched()) {
 
